ff43f4f043d60e4e03490e112c7b48d67365a00f
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-29 11:24:02 -0500

    Add project files.

---
+++ Week4IntroToLinq/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Week4IntroToLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
