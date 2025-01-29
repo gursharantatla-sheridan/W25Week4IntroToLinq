1077bd9ab617cd3cbb1f6011bd66bd30d58b718e
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-29 11:54:53 -0500

    employee class

---
+++ Week4IntroToLinq/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4IntroToLinq
+{
+    public class Employee
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = null!;
+        public double Salary { get; set; }
+
+        public Employee(string firstName, string lastName, double salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName,-10} {LastName,-10} {Salary,10:C}";
+        }
+    }
+}
